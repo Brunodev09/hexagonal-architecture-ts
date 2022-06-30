f8c2735841766bf4ade59dd1f263cecebc3fe37f
Bruno Giannotti <bruno.mayol.giannotti@gmail.com>
2022-06-29 21:08:11 -0300

    [STA-002] - Updating typescript and adding database connections.

--- src/framework/api/server.ts
+++ src/framework/api/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import { RegisterRoutes } from './routes';
 import swaggerDocs from './swagger.json';
 import { ValidateError as TsoaValidationError } from 'tsoa';
@@ -16,7 +17,8 @@ if (!process ||
   !process.env.PORT ||
   !process.env.APPLICATION_NAME ||
   !process.env.ENVIRONMENT_NAME ||
-  !process.env.API_VERSION) throw new CoreError('Environment variables are not set. Please create a .env file at the root of this project.');
+  !process.env.API_VERSION ||
+  !process.env.DB_ADDRESS) throw new CoreError('Environment variables are not set. Please create a .env file at the root of this project.');
 
 /**
  * Instantiating express to create the server for the application.
@@ -25,14 +27,14 @@ const app = express();
 const server = new serverListener(app);
 
 // Configuring expected Errors for the middleware routes.
-  const errorHandlerFactory = Middlewares.getErrorHandlerMiddlewareFactory();
-  errorHandlerFactory.registerExpectedError(400, ValidationError);
-  errorHandlerFactory.registerExpectedError(400, TsoaValidationError, (err: TsoaValidationError): ValidationError => {
-    const fields = Object.keys(err.fields);
-    return new ValidationError(fields);
-  });
-  errorHandlerFactory.registerExpectedError(422, BusinessError);
-  errorHandlerFactory.registerExpectedError(500, CoreError);
+const errorHandlerFactory = Middlewares.getErrorHandlerMiddlewareFactory();
+errorHandlerFactory.registerExpectedError(400, ValidationError);
+errorHandlerFactory.registerExpectedError(400, TsoaValidationError, (err: TsoaValidationError): ValidationError => {
+  const fields = Object.keys(err.fields);
+  return new ValidationError(fields);
+});
+errorHandlerFactory.registerExpectedError(422, BusinessError);
+errorHandlerFactory.registerExpectedError(500, CoreError);
 
 // Registering express middlewares.
 server.middlewares(Middlewares.getRequestResponseMiddlewares(swaggerDocs));
@@ -48,3 +50,25 @@ server.run(Number(process.env.PORT),
   process.env.ENVIRONMENT_NAME,
   process.env.API_VERSION,
   Logger);
+
+mongoose.connect(process.env.DB_ADDRESS);
+mongoose.connection.on("connected", () => {
+  Logger.info(`Mongoose connection event received!`);
+});
+
+mongoose.connection.on("error", err => {
+  Logger.error("Mongoose default connection error: " + err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  Logger.info("Mongoose default connection disconnected");
+});
+
+process.on("SIGINT", () => {
+  mongoose.connection.close(function () {
+    Logger.info(
+      "Mongoose default connection disconnected through app termination"
+    );
+    process.exit(0);
+  });
+});
